35b950593c72e66a80601922458d065997df3065
Jiaqi Qin <57151905+Jiaqi1211@users.noreply.github.com>
2020-12-07 16:55:49 +0100

    Update LQR_controller.m

--- Laborwork2/LQR_controller.m
+++ Laborwork2/LQR_controller.m
@@ -1,11 +1,16 @@
 run('Linearize.m')
 
 p=size(3,1);                    % no. of outputs (y)
-[n,m]=size(B);                  % no. of states nd inputs (u)
-Znm=zeros(n,m); Zmm=zeros(m,m); 
-Znn=zeros(n,n); Zmn=zeros(m,n);
+[n,z]=size(B);                  % no. of states nd inputs (u)(6,2)
+F=zeros(n,z); T=zeros(z,z); 
+W=zeros(n,n); H=zeros(z,n);
 % 1) Design a state feedback controller
-A_aug=[A Znm; -C Zmm]; B_aug=[B;-D];    % augment plant with integrators
-Q=[Znn Znm; Zmn eye(m,m)];      % weight on integrated error
-R=eye(m);                       % input weight
-Kr=lqr(A,B,Q,R);  
+%A_aug=[A F; -C T]; 
+%B_aug=[B;-D];                   % augment plant with integrators
+Q_I=eye(9);             % weight on integrated error
+Q=eye(n);
+R=eye(z);                       % input weight
+[Kr,P,EW]=lqr(A,B,Q,R);
+[Kr_I,P_I,EW_I] = lqi(plant,Q_I,R,0);
+
+u = -Kr*x;
