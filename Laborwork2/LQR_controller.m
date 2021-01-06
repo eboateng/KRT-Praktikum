31b4654ac63a3f107df920b0232fbe1adaefdab9
Jiaqi Qin <57151905+Jiaqi1211@users.noreply.github.com>
2021-01-06 13:08:03 +0100

    Update LQR_controller.m

--- Laborwork2/LQR_controller.m
+++ Laborwork2/LQR_controller.m
@@ -2,17 +2,24 @@ run('Linearize.m')
 
 % Parameter of the LQI_Controller
 
-Q_LQI=eye(8);             % weight on integrated error
-%Q=eye(n);
-R=eye(2);                       % input weight
+
+Q=eye(8);             
+R=eye(2);                       
 C_LQI = [eye(2), zeros(2,4)];
-%[K_LQR,P,EW]=lqr(A,B,Q,R);
-[K_LQI,P_I,EW_I] = lqi(ss(A,B,C_LQI,[]),Q_LQI,R);
+[K_LQI,P_I,EW_I] = lqi(ss(A,B,C_LQI,[]),Q,R);
 K_LQR = K_LQI(1:2,1:6);
+
+% Vorfilter
 Vorfilter = -inv(C_LQI*inv(A-B*K_LQR)*B);
-EW = eig(A-B*K_LQR);
+
+% Neue Zustandsraumdarstellung
+A_LQI=[A,zeros(6,2);-C_LQI,zeros(2,2)]; 
+B_LQI=[B;zeros(2,2)];
+
+% Eigenwerte des geschlossenen Kreises
+EW = eig(A_LQI-B_LQI*K_LQI);
 
 % Oberserver
-observer_poles=[-10 -10 -20 -20 -30 -30]; 
+observer_poles=[-20 -21 -22 -23 -24 -25]; 
 L=place(A',C', observer_poles); 
 L=L'; 
