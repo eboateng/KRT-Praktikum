b3c8f3ca769e6977cd334ca6b794162cbe97ad2b
Jiaqi Qin <57151905+Jiaqi1211@users.noreply.github.com>
2021-02-03 12:38:05 +0100

    Update helikopter.m

--- Laborwork3/helikopter.m
+++ Laborwork3/helikopter.m
@@ -27,10 +27,10 @@ L3 = 355 * 1e-3;
 
 
 %Abstände der Heli-Körperschwerpunkte zum Gelenkpunkt Joint_3
-l_body = (sqrt((355/2)^2 + (41 - 69 - 27/2)^2))*1e-3;
+l_body = (sqrt((355/2)^2 + (69+27/2-41)^2))*1e-3;
 l_motor = (sqrt((355/2)^2 + (41 - 69/2)^2))*1e-3;
 l_bar = (41 + 6/2)*1e-3;
-l_joint3 = (41 - 0.5*19. - 2)*1e-3;
+l_joint3 = (41 - 19/2)*1e-3;
 
 
 %Abstände der Arm-Körperschwerpunkte zum Gelenkpunkt Joint_1 
@@ -54,11 +54,8 @@ theta_c = 2*(m_body*l_body^2 + m_motor*l_motor^2) + m_bar*l_bar^2 + m_joint3*l_j
 theta_b = Summe_heli*l_heli^2 + m_main_arm*l_MainArm^2 + m_second_arm*l_SecArm^2 + ... 
     m_conter_weight*l_Conter^2 + m_magnet*l_Magnet^2 + m_joint1*l_Joint1^2 + m_joint2*l_Joint2^2;
 
-theta_a_1 = Summe_heli*L2^2 + m_main_arm*lG_MainArm^2 + m_second_arm*lG_SecArm^2 + ... 
+theta_a = Summe_heli*L2^2 + m_main_arm*lG_MainArm^2 + m_second_arm*lG_SecArm^2 + ... 
     m_conter_weight*lG_Conter^2 + m_magnet*lG_Magnet^2 + m_joint2*L2^2;
-r_Joint1 = 11*1e-3; 
-theta_a_2 = 0.5*m_joint1*r_Joint1^2 + m_plastic_part*((88*1e-3)^2 + (38*1e-3)^2)/12;
-theta_a = theta_a_1 + theta_a_2;
 
 % Drehmoment in der Beta-Dynamik 
 M_beta = g*(-Summe_heli*L2 - m_main_arm*lG_MainArm + m_second_arm*lG_SecArm + ... 
